b00cfe14ed13b744a2e1924542462d7aa69acda9
DWizard11 <daniyalisawesome111@gmail.com>
2022-10-22 10:07:49 +0800

    Initial Commit

---
+++ GiftHub/GiftHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GiftHubApp.swift
+//  GiftHub
+//
+//  Created by DWizard11 on 22/10/22.
+//
+
+import SwiftUI
+
+@main
+struct GiftHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
