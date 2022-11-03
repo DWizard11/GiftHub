53fe599f6c6d4ee7b1a5b09f1359379ff457bf26
Pixelatt <chloeting77@gmail.com>
2022-11-03 15:36:14 +0800

    code for Birthday Notifications added,
    but idk where exactly to put the code but i just named it and added it for now

---
+++ GiftHub/NotificationView.swift
@@ -0,0 +1,42 @@
+//
+//  NotificationView.swift
+//  GiftHub
+//
+//  Created by T Krobot on 3/11/22.
+//
+
+//here is notif code but idk where to incorporate this in the app help
+import SwiftUI
+
+struct NotificationView: View{
+    @State private var selectedDate = Date()
+    let notify = NotificationHandler()
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            Spacer()
+            
+            DatePicker("Pick a date:", selection: $selectedDate, in: Date()...)
+            Button("Schedule notification") {
+                notify.sendNotification(date: selectedDate,
+                                        type: "date",
+                                        title: "Hello!",
+                                        body: "Your Friend's Birthday is coming!")
+            }.tint(.orange)
+            
+            Spacer()
+            Text("Not working?")
+                .foregroundColor(.gray)
+                .italic()
+            Button("Request permissions") {
+                notify.askPermission()
+            }
+        }
+        .padding()
+    }
+}
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
