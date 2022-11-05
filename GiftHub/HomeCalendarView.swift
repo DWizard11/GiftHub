0d42be4b09c5ef6d90d1c05da5fcbf383ff69a4e
DWizard11 <daniyalisawesome111@gmail.com>
2022-11-05 11:17:12 +0800

    Added colour to HomeCalendarView and added Upcoming Birthdays - Felix

--- GiftHub/HomeCalendarView.swift
+++ GiftHub/HomeCalendarView.swift
@@ -7,19 +7,69 @@
 
 import SwiftUI
 
+extension UIColor {
+   convenience init(red: Int, green: Int, blue: Int) {
+       assert(red >= 0 && red <= 255, "Invalid red component")
+       assert(green >= 0 && green <= 255, "Invalid green component")
+       assert(blue >= 0 && blue <= 255, "Invalid blue component")
+
+       self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+   }
+
+   convenience init(rgb: Int) {
+       self.init(
+           red: (rgb >> 16) & 0xFF,
+           green: (rgb >> 8) & 0xFF,
+           blue: rgb & 0xFF
+       )
+   }
+}
+let color2 = UIColor(red: 255, green: 248, blue: 211)
+let color3 = Color(red: 255/255, green: 242/255, blue: 172/255)
+let color4 = Color(red: 255/255, green: 255/255, blue: 109/255)
+
 struct HomeCalendarView: View {
+    
     @State private var date = Date()
+    
     var body: some View {
-        VStack {
-            HStack{
-                Text("Birthday Calendar")
-                    .font(.largeTitle)
-                Image(systemName: "plus.circle.fill")
-                    .font(.system(size: 15))
+        ZStack {
+            Color(color2)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                HStack{
+                    Text("Birthday Calendar")
+                        .font(.largeTitle)
+                        .bold()
+                    Image(systemName: "plus.circle.fill")
+                        .font(.system(size: 30))
+                        .foregroundColor(.yellow)
+                    
+                }
+                DatePicker("Enter your birthday", selection: $date)
+                    .datePickerStyle(GraphicalDatePickerStyle())
+                    .frame(maxHeight: 400)
+                    .background(.white)
+                    .padding()
+                
+                ZStack{
+                    RoundedRectangle(cornerRadius: 30)
+                        .foregroundColor(color3)
+                        .padding()
+                    VStack{
+                        Text("Upcoming Birthdays!")
+                            .font(.largeTitle)
+                            .bold()
+                            .padding()
+                        RoundedRectangle(cornerRadius: 30)
+                            .foregroundColor(color4)
+                            .padding()
+                        RoundedRectangle(cornerRadius: 30)
+                            .foregroundColor(color4)
+                            .padding()
+                    }
+                }
             }
-            DatePicker("Enter your birthday", selection: $date)
-                .datePickerStyle(GraphicalDatePickerStyle())
-                .frame(maxHeight: 400)
         }
     }
 }
@@ -29,3 +79,4 @@ struct HomeCalendarView_Previews: PreviewProvider {
         HomeCalendarView()
     }
 }
+
