cec549a1a5df2d5df06c0cb94fa5b1e8b09b53f9
Zhiyu <zhiyuma2006@gmail.com>
2022-11-13 18:14:35 +0800

    added friend detail view (not done)

--- GiftHub/FriendDetailView.swift
+++ GiftHub/FriendDetailView.swift
@@ -8,10 +8,52 @@
 import SwiftUI
 
 struct FriendDetailView: View {
+    
+    @State var friendbirthday = ""
+    @State var dates = ["1 day", "4 days", "1 week", "1 month"]
+    @State var selectedDate = "1 week"
+    @State var friendname = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Form {
+                Section("Friend Name") {
+                    TextField("Friend Name", text: $friendname)
+                }
+                Section("Friend Birthday") {
+                    TextField("Birthday", text: $friendbirthday)
+                       
+                    }
+                //
+                Section("Alert") {
+                    Picker("Notification", selection: $selectedDate) {
+                        ForEach(dates, id: \.self) { date in
+                            Text(date)
+                        }
+                    }
+                }
+                //
+                Section("Friend Likings") {
+                    NavigationLink("Add Friend Likings") {
+                        MainFriendLikingView()
+                    }
+                }
+                Section("Friend Dislikes") {
+                    NavigationLink("Add Friend Dislikes") {
+                        MainFriendDislikesView()
+                    }
+                }
+                Section("Gift Ideas") {
+                    NavigationLink("Add Gift Ideas") {
+                        MainGiftIdeaView()
+                    }
+                }
+                //
+            }
+        }
     }
 }
+
 
 struct FriendDetailView_Previews: PreviewProvider {
     static var previews: some View {
