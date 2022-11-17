52a3c9379947ad12d950358a6f32ea641e2e4a9b
Felbadatcoding <100569717+Felbadatcoding@users.noreply.github.com>
2022-11-17 23:31:19 +0800

    Added placeholder display for upcoming bdays

--- GiftHub/HomeCalendarView.swift
+++ GiftHub/HomeCalendarView.swift
@@ -1,15 +1,15 @@
 import SwiftUI
 
 extension UIColor {
-    init(red: Int, green: Int, blue: Int) {
+   convenience init(red: Int, green: Int, blue: Int) {
        assert(red >= 0 && red <= 255, "Invalid red component")
        assert(green >= 0 && green <= 255, "Invalid green component")
        assert(blue >= 0 && blue <= 255, "Invalid blue component")
 
-       self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0)
+       self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
    }
 
-    init(rgb: Int) {
+   convenience init(rgb: Int) {
        self.init(
            red: (rgb >> 16) & 0xFF,
            green: (rgb >> 8) & 0xFF,
@@ -17,7 +17,7 @@ extension UIColor {
        )
    }
 }
-let color2 = Color(red: 255, green: 248, blue: 211)
+let color2 = UIColor(red: 255, green: 248, blue: 211)
 let color3 = Color(red: 255/255, green: 242/255, blue: 172/255)
 let color4 = Color(red: 255/255, green: 255/255, blue: 109/255)
 
@@ -46,29 +46,85 @@ struct HomeCalendarView: View {
                     Text("Upcoming Birthdays!")
                         .font(.largeTitle)
                         .bold()
-                    RoundedRectangle(cornerRadius: 30)
-                        .foregroundColor(color4)
-                        .frame(width: 1000, height: 70)
-                    RoundedRectangle(cornerRadius: 30)
-                        .foregroundColor(color4)
-                        .padding()
-                        .frame(width: 1000, height: 100)
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 15)
+                            .foregroundColor(color4)
+                            .frame(width: 350, height: 75)
+                        HStack{
+                            Circle()
+                                .fill(.white)
+                                .frame(width: 100, height: 50)
+                                    
+                            VStack {
+                                Text("Placehold")
+                                Text("3 Nov 2022")
+                            }
+                            }
+                        }
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 15)
+                            .foregroundColor(color4)
+                            .frame(width: 350, height: 75)
+                        HStack{
+                            Circle()
+                                .fill(.white)
+                                .frame(width: 100, height: 50)
+                                    
+                            VStack {
+                                Text("Placehold")
+                                Text("3 Nov 2022")
+                            }
+                            }
+                        }
+                    Text("Next Week                                   ")
+                        .font(.title)
+                        .bold()
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 15)
+                            .foregroundColor(color4)
+                            .frame(width: 350, height: 75)
+                        HStack{
+                            Circle()
+                                .fill(.white)
+                                .frame(width: 100, height: 50)
+                                    
+                            VStack {
+                                Text("Placehold")
+                                Text("3 Nov 2022")
+                            }
+                            }
+                        }
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 15)
+                            .foregroundColor(color4)
+                            .frame(width: 350, height: 75)
+                        HStack{
+                            Circle()
+                                .fill(.white)
+                                .frame(width: 100, height: 50)
+                                    
+                            VStack {
+                                Text("Placehold")
+                                Text("3 Nov 2022")
+                            }
+                            }
+                        }
+                    Button(role: .none){
+                                    isSheetShown = true
+                                } label: {
+                                    Text("Add a Reminder")
+                                    Text(Image(systemName: "plus.circle.fill"))
+                                        .font(.system(size: 30))
+                                        .foregroundColor(.yellow)
+                    }
                 }
-                Button(role: .none){
-                    isSheetShown = true
-                } label: {
-                    Text("Add a Reminder")
-                    Text(Image(systemName: "plus.circle.fill"))
-                        .font(.system(size: 30))
-                        .foregroundColor(.yellow)
-                }
-            }
-        }
+                            }
+                        }
         .sheet(isPresented: $isSheetShown){
             NotificationView(isPresented: $isSheetShown)
-        }
-    }
-}
+                    }
+                    }
+                }
 
 struct HomeCalendarView_Previews: PreviewProvider {
     static var previews: some View {
