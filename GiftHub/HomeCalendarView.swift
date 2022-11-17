7ea475cbfe9efc0e5eb42844855e29b08a7592f1
Felbadatcoding <100569717+Felbadatcoding@users.noreply.github.com>
2022-11-17 22:10:37 +0800

    Changed extension name "Color" to "UIColor"
    an error was caused when the name was "Color" so i changed it to "UIColor"

--- GiftHub/HomeCalendarView.swift
+++ GiftHub/HomeCalendarView.swift
@@ -1,6 +1,6 @@
 import SwiftUI
 
-extension Color {
+extension UIColor {
     init(red: Int, green: Int, blue: Int) {
        assert(red >= 0 && red <= 255, "Invalid red component")
        assert(green >= 0 && green <= 255, "Invalid green component")
